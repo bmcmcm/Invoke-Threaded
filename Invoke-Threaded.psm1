808385a6e54e7a7f2f10ab85d3ac0e779008ce7d
bmcmcm <brian@net70>
2023-02-05 14:20:44 -0800

    use $PWD for the working directory

--- Invoke-Threaded.psm1
+++ Invoke-Threaded.psm1
@@ -1,15 +1,14 @@
-$Scripts = Get-ChildItem "$($PSScriptRoot)\public-functions\*.ps1" | Select-Object -ExpandProperty FullName
-$Scripts += Get-ChildItem "$($PSScriptRoot)\private-functions\*.ps1" | Select-Object -ExpandProperty FullName
-
+$Scripts = Get-ChildItem "$PWD\public-functions\*.ps1" | Select-Object -ExpandProperty FullName
+$Scripts += Get-ChildItem "$PWD\private-functions\*.ps1" | Select-Object -ExpandProperty FullName
 foreach ($script in $Scripts)
 {
     try 
     {
-        Write-Verbose $script
-        . $($script)
+        Write-Host $script
+        . $script
     }
     catch 
     {
-        Write-Verbose ("{0}: {1}" -f $script,$_.Exception.Message) 
+        Write-Host ("{0}: {1}" -f $script,$_.Exception.Message) 
     }
 }
