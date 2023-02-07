6fdf5bcfe508dc3764ace0c90d9204ff0d3ffd28
bmcmcm <brian@net70>
2023-02-06 16:44:07 -0800

    Updated psm1 with old path method

--- Invoke-Threaded.psm1
+++ Invoke-Threaded.psm1
@@ -1,18 +1,15 @@
-$pub = (Join-Path $PSScriptRoot -ChildPath "public-functions")
-$priv = = (Join-Path $PSScriptRoot -ChildPath "private-functions")
-
-[string[]]$Scripts = Get-ChildItem -File -Recurse -LiteralPath $pub -Filter *.ps1 | Select-Object -ExpandProperty FullName
-$Scripts += Get-ChildItem -File -Recurse -LiteralPath $priv -Filter *.ps1 | Select-Object -ExpandProperty FullName
+$Scripts = Get-ChildItem "$($PSScriptRoot)\public-functions\*.ps1" | Select-Object -ExpandProperty FullName
+$Scripts += Get-ChildItem "$($PSScriptRoot)\private-functions\*.ps1" | Select-Object -ExpandProperty FullName
 
 foreach ($script in $Scripts)
 {
     try 
     {
-        Write-Host $script
-        . $script
+        Write-Verbose $script
+        . $($script)
     }
     catch 
     {
-        Write-Host ("{0}: {1}" -f $script,$_.Exception.Message) 
+        Write-Verbose ("{0}: {1}" -f $script,$_.Exception.Message)
     }
 }
